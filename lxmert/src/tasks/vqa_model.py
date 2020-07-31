46044d13a7505b0aac0caec9c768be9294e7bdb8
Nikhil <shnik456@iitkgp.ac.in>
2020-07-31 01:47:08 -0700

    passing strategy and policy in constructor and forward

--- lxmert/src/tasks/vqa_model.py
+++ lxmert/src/tasks/vqa_model.py
@@ -12,13 +12,16 @@ MAX_VQA_LENGTH = 20
 
 
 class VQAModel(nn.Module):
-    def __init__(self, num_answers):
+    def __init__(self, num_answers, finetune_strategy='standard'):
         super().__init__()
+
+        #self.finetune_strategy = finetune_strategy
         
         # Build LXRT encoder
         self.lxrt_encoder = LXRTEncoder(
             args,
-            max_seq_length=MAX_VQA_LENGTH
+            max_seq_length=MAX_VQA_LENGTH,
+            finetune_strategy=finetune_strategy
         )
         hid_dim = self.lxrt_encoder.dim
         
@@ -31,7 +34,7 @@ class VQAModel(nn.Module):
         )
         self.logit_fc.apply(self.lxrt_encoder.model.init_bert_weights)
 
-    def forward(self, feat, pos, sent):
+    def forward(self, feat, pos, sent, policy=None):
         """
         b -- batch_size, o -- object_number, f -- visual_feature_size
 
@@ -41,7 +44,7 @@ class VQAModel(nn.Module):
         :param leng: (b,) Type -- int numpy array
         :return: (b, num_answer) The logit of each answers.
         """
-        x = self.lxrt_encoder(sent, (feat, pos))
+        x = self.lxrt_encoder(sent, (feat, pos), policy)
         logit = self.logit_fc(x)
 
         return logit
