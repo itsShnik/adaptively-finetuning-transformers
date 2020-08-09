bca64452dea5a60fb0bc854a5ae44193886bf95f
Nikhil <shnik456@iitkgp.ac.in>
2020-08-08 22:04:16 -0700

    inefficient implementation of global policy, no actual performance improvement but does the same job

--- VL-BERT/vqa/function/val.py
+++ VL-BERT/vqa/function/val.py
@@ -13,12 +13,19 @@ PolicyVec = {'SpotTune':180,
 vis = VisualizationPlotter()
 
 @torch.no_grad()
-def do_validation(net, val_loader, metrics, label_index_in_batch, epoch_num=0, finetune_strategy='standard', policy_net=None, policy_optimizer=None):
+def do_validation(net, val_loader, metrics, label_index_in_batch, epoch_num=0, finetune_strategy='standard', policy_net=None, policy_optimizer=None, global_decision=False, policy_decisions=None, policy_total=None):
     net.eval()
     if finetune_strategy in PolicyVec:
         policy_net.eval()
         policy_save = torch.zeros(PolicyVec[finetune_strategy]).cpu()
         policy_max = 0
+
+        # check if we have to make a global decision
+        if global_decision:
+            # calculate the policy
+            policy_decisions = policy_decisions / policy_total
+            policy_init = (policy_decisions > 0.5).float()
+
 
     metrics.reset()
     for nbatch, batch in enumerate(val_loader):
@@ -27,9 +34,13 @@ def do_validation(net, val_loader, metrics, label_index_in_batch, epoch_num=0, f
         datas = [batch[i] for i in range(len(batch)) if i != label_index_in_batch % len(batch)]
 
         if finetune_strategy in PolicyVec:
-            policy_vector = policy_net(*datas)
-            policy_action = gumbel_softmax(policy_vector.view(policy_vector.size(0), -1, 2))
-            policy = policy_action[:,:,1]
+            if not global_decision:
+                policy_vector = policy_net(*datas)
+                policy_action = gumbel_softmax(policy_vector.view(policy_vector.size(0), -1, 2))
+                policy = policy_action[:,:,1]
+            else:
+                # repeat to match the batch size
+                policy = policy_init.repeat(batch[1].size(0), 1)
             policy_save = policy_save + policy.clone().detach().cpu().sum(0)
             policy_max += policy.size(0)
             outputs = net(*datas, policy)
