e50204ff47bfba897ce5f885913122ac5304c8d0
Nikhil <shnik456@iitkgp.ac.in>
2020-08-13 20:36:27 -0700

    implementation of log fraction loss

--- VL-BERT/common/auxiliary_losses.py
+++ VL-BERT/common/auxiliary_losses.py
@@ -32,7 +32,30 @@ def deterministic_policy_loss(policy, scale):
     fraction = fraction + 1e-9
 
     # calculate the loss
-    loss = (-1 * fraction * torch.log(fraction)).sum(0).mean(0)
+    loss = (-1 * fraction * torch.log(fraction)).sum(0)
+
+    # scale the loss
+    loss *= scale
+
+    return loss
+
+def log_fraction_loss(policy, scale):
+    """
+    Policy: Action Vector
+    Scale: The multiplying factor for loss
+    """
+
+    # Find the fraction of blocks used per example
+    fraction = policy.mean(-1)
+
+    # Find the squared fraction
+    sq_fraction = fraction ** 2
+
+    # Subtract from 1 because we have to minimize the sq_fraction
+    sq_fraction = 1 - sq_fraction
+
+    # Take log and sum
+    loss = -(torch.log(sq_fraction).sum(0))
 
     # scale the loss
     loss *= scale
