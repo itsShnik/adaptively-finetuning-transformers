42a2b48c63858e1ece486f8852e01670885d4986
Nikhil <shnik456@iitkgp.ac.in>
2020-08-14 19:55:51 -0700

    PolicyVec includes BlockReplace

--- VL-BERT/common/trainer.py
+++ VL-BERT/common/trainer.py
@@ -15,7 +15,8 @@ except ImportError:
 # policy vector shapes
 PolicyVec = {'SpotTune':180,
         'SpotTune_Block':12,
-        'BlockDrop':12}
+        'BlockDrop':12,
+        'BlockReplace':12}
 
 # Parameter to pass to batch_end_callback
 BatchEndParam = namedtuple('BatchEndParams',
